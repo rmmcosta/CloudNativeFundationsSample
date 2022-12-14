87e2803636b7ce99b0e6f228a518643e914b3119
rmmcosta <ricardocosta101085@gmail.com>
2022-12-14 22:38:57 +0000

    feat:Added logs and metrics and status endpoints to the python hello world exercise

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,60 @@
 from flask import Flask
+import logging
+import logging.config
+import yaml
+from datetime import datetime
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
+    logging_endpoint("hello")
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+@app.route("/status")
+def status():
+    logging_endpoint("status")
+    return {
+        "result": "OK - healthy"
+    }
+    # return Response("{result: OK - healthy}", mimetype="application/json")
+
+
+@app.route("/metrics")
+def metrics():
+    logging_endpoint("metrics")
+    return {
+        "data": {
+            "UserCount": 140,
+            "UserCountActive": 23
+        }
+    }
+    # return Response("{data: {UserCount: 140, UserCountActive: 23}}", mimetype="application/json")
+
+
+def logging_endpoint(enpoint_name):
+    app.logger.info("%s, %s endpoint was reached",
+                    datetime.now(), enpoint_name)
+
+
+# if __name__ == "__main__":
+#     logging.config.dictConfig(yaml.load(open('logging.conf')))
+#     logfile = logging.getLogger('file')
+#     logconsole = logging.getLogger('console')
+#     logfile.debug("Debug FILE")
+#     logconsole.debug("Debug CONSOLE")
+#     from waitress import serve
+#     serve(app, host="0.0.0.0", port=5000)
+    #app.run(host='0.0.0.0')
+
+#https://stackoverflow.com/questions/51025893/flask-at-first-run-do-not-use-the-development-server-in-a-production-environmen
+def create_app():
+    logging.config.dictConfig(yaml.load(open('logging.conf')))
+    logfile = logging.getLogger('file')
+    logconsole = logging.getLogger('console')
+    logfile.debug("Debug FILE")
+    logconsole.debug("Debug CONSOLE")
+    #waitress-serve --port=5000 --call app:create_app
+    return app
